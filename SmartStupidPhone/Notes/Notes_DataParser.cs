dfc9e58967232e6e8e4a9fb3ac08fa72dbc718aa
Andrew Barker <androidboy@gmail.com>
2013-05-07 18:20:16 -0400

    *first try at presentation starting point

---
+++ SmartStupidPhone/Notes/Notes_DataParser.cs
@@ -0,0 +1,51 @@
+﻿/*
+            if(dateString.Length < 12)
+            {
+                return null;
+            }
+
+            int year = 0;
+            if (!int.TryParse(dateString.Substring(0,4), out year))
+            {
+                return null;
+            }
+
+            int month = 0;
+            if (!int.TryParse(dateString.Substring(4, 2), out month))
+            {
+                return null;
+            }
+
+            int day = 0;
+            if (!int.TryParse(dateString.Substring(6, 2), out day))
+            {
+                return null;
+            }
+
+            int hour = 0;
+            if (!int.TryParse(dateString.Substring(8, 2), out hour))
+            {
+                return null;
+            }
+
+            int minute = 0;
+            if(!int.TryParse(dateString.Substring(10,2), out minute))
+            {
+                return null;
+            }
+
+            DateTime parsedDateTime;
+
+            //todo: DEMONSTRATE: defect-finding: get rid of this try/catch, and see one of the tests fail
+            try
+            {
+                parsedDateTime = new DateTime(year, month, day, hour, minute, 0, 0);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                return null;
+            }
+
+            return parsedDateTime;
+        }
+*/
--- SmartStupidPhone/StupidSmartPhone.Core/DateParser.cs
+++ SmartStupidPhone/StupidSmartPhone.Core/DateParser.cs
@@ -15,59 +15,7 @@ namespace StupidSmartPhone.Core
         /// <returns></returns>
         public DateTime? GetDateTimeFromString(string dateString)
         {
-            //todo: DEMONSTRATE: test-first:  show this method as throwing NotImplemented Exception, then write implementation
-            
-            //todo: DEMONSTRATE: refactoring: show tests passing with code as-is
-            //then abstract things out, use unit tests to ensure it still works            
-                                    
-            if(dateString.Length < 12)
-            {
-                return null;
-            }
-
-            int year = 0;
-            if (!int.TryParse(dateString.Substring(0,4), out year))
-            {
-                return null;
-            }
-
-            int month = 0;
-            if (!int.TryParse(dateString.Substring(4, 2), out month))
-            {
-                return null;
-            }
-
-            int day = 0;
-            if (!int.TryParse(dateString.Substring(6, 2), out day))
-            {
-                return null;
-            }
-
-            int hour = 0;
-            if (!int.TryParse(dateString.Substring(8, 2), out hour))
-            {
-                return null;
-            }
-
-            int minute = 0;
-            if(!int.TryParse(dateString.Substring(10,2), out minute))
-            {
-                return null;
-            }
-
-            DateTime parsedDateTime;
-
-            //todo: DEMONSTRATE: defect-finding: get rid of this try/catch, and see one of the tests fail
-            try
-            {
-                parsedDateTime = new DateTime(year, month, day, hour, minute, 0, 0);
-            }
-            catch (System.ArgumentOutOfRangeException e)
-            {
-                return null;
-            }
-
-            return parsedDateTime;
+            throw new NotImplementedException();
         }
     }
 }
