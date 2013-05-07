df0a9d6cb938de1961b7c36f95e2044c8c74a587
Andrew Barker <androidboy@gmail.com>
2013-05-07 18:08:06 -0400

    Whole project in good shape, no external interactions (google) working

---
+++ SmartStupidPhone/GoogleInteractor/GMailReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExternalInteractor
+{
+    class GMailReader : IMailReader
+    {
+        public string GetEarliestUnreadTranscript()
+        {
+            
+            
+            
+            return string.Empty;
+        }
+    
+    }
+}
