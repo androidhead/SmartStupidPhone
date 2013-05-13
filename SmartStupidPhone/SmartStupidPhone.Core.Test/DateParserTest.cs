da7400dc634157adfab93816d55ced9e063d416d
Andrew Barker <androidboy@gmail.com>
2013-05-13 13:41:48 -0400

    changes to incorporate into a 4th starting point
    from meeting with Seth today

---
+++ SmartStupidPhone/SmartStupidPhone.Core.Test/DateParserTest.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StupidSmartPhone.Core;
+
+namespace SmartStupidPhone.Core.Test
+{
+    [TestClass]
+    public class DateParserTest
+    {
+        [TestMethod]
+        public void GetDateFromString_Basic_Success()
+        {
+            string input = "201305030101";
+
+            var dateParser = new DateParser();
+            DateTime? output = dateParser.GetDateTimeFromString(input);
+
+            Assert.AreEqual(new DateTime(2013, 5, 3, 1, 1, 0), output.Value);
+        }
+        
+        //handle any valid date (valid defined by DateTime object)
+        //return null if dates are invalid                 
+        [TestMethod]
+        public void GetDateFromString_MinDate_Success()
+        {
+            var inputDateValue = DateTime.MinValue;
+
+            string dateParserInput = String.Concat(
+                inputDateValue.Year.ToString().PadLeft(4, '0'),
+                inputDateValue.Month.ToString().PadLeft(2, '0'),
+                inputDateValue.Day.ToString().PadLeft(2, '0'),
+                inputDateValue.Hour.ToString().PadLeft(2, '0'),
+                inputDateValue.Minute.ToString().PadLeft(2, '0'));
+
+            var dateParser = new DateParser();
+            DateTime? output = dateParser.GetDateTimeFromString(dateParserInput);
+
+            Assert.AreEqual(inputDateValue.Year, output.Value.Year);
+            Assert.AreEqual(inputDateValue.Month, output.Value.Month);
+            Assert.AreEqual(inputDateValue.Day, output.Value.Day);
+            Assert.AreEqual(inputDateValue.Minute, output.Value.Minute);
+            Assert.AreEqual(inputDateValue.Hour, output.Value.Hour);
+        }
+
+        [TestMethod]
+        public void GetDateFromString_MaxDate_Success()
+        {
+            var inputDateValue = DateTime.MinValue;
+
+            string dateParserInput = String.Concat(
+                inputDateValue.Year.ToString().PadLeft(4, '0'),
+                inputDateValue.Month.ToString().PadLeft(2, '0'),
+                inputDateValue.Day.ToString().PadLeft(2, '0'),
+                inputDateValue.Hour.ToString().PadLeft(2, '0'),
+                inputDateValue.Minute.ToString().PadLeft(2, '0'));
+
+            var dateParser = new DateParser();
+            DateTime? output = dateParser.GetDateTimeFromString(dateParserInput);
+
+            Assert.AreEqual(inputDateValue.Year, output.Value.Year);
+            Assert.AreEqual(inputDateValue.Month, output.Value.Month);
+            Assert.AreEqual(inputDateValue.Day, output.Value.Day);
+            Assert.AreEqual(inputDateValue.Minute, output.Value.Minute);
+            Assert.AreEqual(inputDateValue.Hour, output.Value.Hour);
+        }
+
+        [TestMethod]
+        public void GetDateFromString_NullInput_Null()
+        {
+            string input = null;
+
+            var dateParser = new DateParser();
+            DateTime? output = dateParser.GetDateTimeFromString(input);
+
+            Assert.IsNull(output);
+        }
+      
+        [TestMethod]
+        public void GetDateFromString_RandomDate_Success()
+        {
+            var inputDateValue = DateTime.Now;
+
+            string dateParserInput = String.Concat(
+                inputDateValue.Year.ToString().PadLeft(4, '0'),
+                inputDateValue.Month.ToString().PadLeft(2, '0'),
+                inputDateValue.Day.ToString().PadLeft(2, '0'),
+                inputDateValue.Hour.ToString().PadLeft(2, '0'),
+                inputDateValue.Minute.ToString().PadLeft(2, '0'));
+
+            var dateParser = new DateParser();
+            DateTime? output = dateParser.GetDateTimeFromString(dateParserInput);
+
+            Assert.AreEqual(inputDateValue.Year, output.Value.Year);
+            Assert.AreEqual(inputDateValue.Month, output.Value.Month);
+            Assert.AreEqual(inputDateValue.Day, output.Value.Day);
+            Assert.AreEqual(inputDateValue.Minute, output.Value.Minute);
+            Assert.AreEqual(inputDateValue.Hour, output.Value.Hour);
+        }
+
+        [TestMethod]
+        public void GetDateFromString_SlightlyInvalidDate_Null()
+        {
+            string dateParserInput = "200002301200";
+
+            var dateParser = new DateParser();
+            DateTime? output = dateParser.GetDateTimeFromString(dateParserInput);
+
+            Assert.IsFalse(output.HasValue);
+        }
+
+        [TestMethod]
+        public void GetDateFromString_EmptyInput_Null()
+        {
+            string dateParserInput = String.Empty;
+
+            var dateParser = new DateParser();
+            DateTime? output = dateParser.GetDateTimeFromString(dateParserInput);
+
+            Assert.IsFalse(output.HasValue);
+        }
+
+        [TestMethod]
+        public void GetDateFromString_VeryInvalidDate_Null()
+        {
+            string dateParserInput = "this is not nearly a date";
+
+            var dateParser = new DateParser();
+            DateTime? output = dateParser.GetDateTimeFromString(dateParserInput);
+
+            Assert.IsFalse(output.HasValue);
+        }
+    }
+}
